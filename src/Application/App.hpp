6eab8734e92cf9cd418428f269d3fbb6c58f39d4
Nathan Jesudason <nathan.jesudason@gmail.com>
2023-08-01 18:48:34 -0700

    Beginning of API and Wifi implementation

--- src/Application/App.hpp
+++ src/Application/App.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector> //must be defined before KPServer is included, else error
 #include <KPController.hpp>
+#include <KPFileLoader.hpp>
 #include <KPServer.hpp>
 
 
@@ -8,6 +9,7 @@ class App : public KPController {
 private:
   void setupServerRouting();
 public:
+  KPFileLoader fileLoader{"file-loader", 10}; //SDCS is 10 for Atmel M0
   KPServer server{"web-server", "subsampler", "ilab_sampler"};
   void setup() override {
     Serial.begin(115200);
