a6941a36bfc262c04816a3965473889742a99898
Nathan Jesudason <nathan.jesudason@gmail.com>
2023-11-06 11:44:35 -0800

    Update sampler to 100 bar sensor

--- src/Components/PressureSensor.hpp
+++ src/Components/PressureSensor.hpp
@@ -3,14 +3,14 @@
 #include <KPSubject.hpp>
 #include <Components/PressureSensorObserver.hpp>
 #include <Wire.h>
-#include "MS5837.h"
+#include "KellerLD.h"
 #include <KPFoundation.hpp>
 
 class PressureSensor : public KPComponent, public KPSubject<PressureSensorObserver> {
 
   public:
     PressureSensor(const char * name) : KPComponent(name) {}
-    MS5837 sensor;
+    KellerLD sensor;
     unsigned long updateTime;
 
     void setup() override {
@@ -19,21 +19,15 @@ class PressureSensor : public KPComponent, public KPSubject<PressureSensorObserv
       Wire.begin();
 
       // Initialize pressure sensor
-      // Returns true if initialization was successful
-      // We can't continue with the rest of the program unless we can initialize the sensor
-      while (!sensor.init()) {
-        println("Init failed!");
-        println("Are SDA/SCL connected correctly?");
-        println("Blue Robotics Bar30: White=SDA, Green=SCL");
-        println("\n\n\n");
-        delay(5000);
+      sensor.init();
+
+      sensor.setFluidDensity(1029); // kg/m^3 (freshwater, 1029 for seawater)
+
+      if(sensor.isInitialized()) {
+        println("Sensor connected.");
+      } else {
+        println("Sensor not connected.");
       }
-
-      // .init sets the sensor model for us but we can override it if required.
-      // Uncomment the next line to force the sensor model to the MS5837_30BA.
-      //sensor.setModel(MS5837::MS5837_30BA);
-
-      sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
     }
 
   void update() override {
